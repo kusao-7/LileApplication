6323fdbfca5804e1322663527231896f36a069f1
Atsuki Sekine <atski.1010@gmail.com>
2025-06-06 15:38:01 +0900

    試作

---
+++ src/main/java/com/example/LileApplication/LileApplication.java
@@ -0,0 +1,13 @@
+package com.example.LileApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LileApplication/LileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LileApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
